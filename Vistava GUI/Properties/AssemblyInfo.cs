b1a5d74b6fccfc48477bf5c095de1a349daded2c
Maximilian Bauer <bauermaximilian@users.noreply.github.com>
2021-08-02 23:26:47 +0200

    Update license year and version number

--- Vistava GUI/Properties/AssemblyInfo.cs
+++ Vistava GUI/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Vistava")]
-[assembly: AssemblyCopyright("Copyright © Maximilian Bauer 2020")]
+[assembly: AssemblyCopyright("Copyright © Maximilian Bauer 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
